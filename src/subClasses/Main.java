5b4b3ca5e56663527383952eaade8cf6e5eb19f0
KuffourJason <KuffourJason@outlook.com>
2015-11-18 00:06:50 -0500

    main class

---
+++ src/subClasses/Main.java
@@ -0,0 +1,16 @@
+package subClasses;
+
+
+public class Main {
+
+	public static void main( String args[]){
+		
+		
+		//args[0] - student id
+		//args[1] - class room id
+		//args[2] - course id
+		Facade test = new Facade(args[0]);
+		test.updateStudentInstance(args[1], args[2]);
+	}
+	
+}
